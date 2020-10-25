7925b0309070601ab819aea34cb6c3a5572b4b2a
Joel Gustafson <joelg@mit.edu>
2020-10-25 17:14:45 -0400

    Added xsd binary and rdf json datatypes

--- lib/IRIs.d.ts
+++ lib/IRIs.d.ts
@@ -6,6 +6,8 @@ export declare const xsd: {
     string: "http://www.w3.org/2001/XMLSchema#string";
     date: "http://www.w3.org/2001/XMLSchema#date";
     dateTime: "http://www.w3.org/2001/XMLSchema#dateTime";
+    hexBinary: "http://www.w3.org/2001/XMLSchema#hexBinary";
+    base64Binary: "http://www.w3.org/2001/XMLSchema#base64Binary";
 };
 export declare const rdf: {
     type: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
@@ -13,4 +15,5 @@ export declare const rdf: {
     first: "http://www.w3.org/1999/02/22-rdf-syntax-ns#first";
     rest: "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest";
     langString: "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString";
+    JSON: "http://www.w3.org/1999/02/22-rdf-syntax-ns#JSON";
 };
--- src/IRIs.ts
+++ src/IRIs.ts
@@ -3,13 +3,17 @@ const xsdBoolean = "http://www.w3.org/2001/XMLSchema#boolean"
 const xsdDouble = "http://www.w3.org/2001/XMLSchema#double"
 const xsdInteger = "http://www.w3.org/2001/XMLSchema#integer"
 const xsdString = "http://www.w3.org/2001/XMLSchema#string"
+const xsdDate = "http://www.w3.org/2001/XMLSchema#date"
+const xsdDateTime = "http://www.w3.org/2001/XMLSchema#dateTime"
+const xsdHexBinary = "http://www.w3.org/2001/XMLSchema#hexBinary"
+const xsdBase64Binary = "http://www.w3.org/2001/XMLSchema#base64Binary"
+
 const rdfType = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 const rdfNil = "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil"
 const rdfFirst = "http://www.w3.org/1999/02/22-rdf-syntax-ns#first"
 const rdfRest = "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest"
 const rdfLangString = "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString"
-const xsdDate = "http://www.w3.org/2001/XMLSchema#date"
-const xsdDateTime = "http://www.w3.org/2001/XMLSchema#dateTime"
+const rdfJSON = "http://www.w3.org/1999/02/22-rdf-syntax-ns#JSON"
 
 export const xsd = {
 	decimal: xsdDecimal as typeof xsdDecimal,
@@ -19,6 +23,8 @@ export const xsd = {
 	string: xsdString as typeof xsdString,
 	date: xsdDate as typeof xsdDate,
 	dateTime: xsdDateTime as typeof xsdDateTime,
+	hexBinary: xsdHexBinary as typeof xsdHexBinary,
+	base64Binary: xsdBase64Binary as typeof xsdBase64Binary,
 }
 
 export const rdf = {
@@ -27,4 +33,5 @@ export const rdf = {
 	first: rdfFirst as typeof rdfFirst,
 	rest: rdfRest as typeof rdfRest,
 	langString: rdfLangString as typeof rdfLangString,
+	JSON: rdfJSON as typeof rdfJSON,
 }
